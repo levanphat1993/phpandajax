4d800ec1ece5e329fdd31f5236eb2a5e4c0fc2c3
DESKTOP-6GUG76T\Amdin <levanphatit1993@gmail.com>
2022-08-02 17:49:55 +0700

    timeofaction v1

--- quickstart/quickstart.js
+++ quickstart/quickstart.js
@@ -1,11 +1,11 @@
 // stores the reference to the XMLHttpRequest object
 var xmlHttp = createXmlHttpRequestObject();
 
-function createXmlHttpRequestObject
+function createXmlHttpRequestObject()
 {
 	// stores the reference to the XMLHttpRequest Object
 	var xmlHttp;
-	
+
 	// if running Internet Explorer 6 or older
 	if (window.ActiveXObject) {
 		try {
@@ -29,46 +29,52 @@ function createXmlHttpRequestObject
 	}
 }
 
-function process
+function process()
 {
 	// proceed only if the xmlHttp object isn't busy
-	if (xmlHttp.readyState == 4 || xmlHttp.readyState == 0 ) {
-		
-		// retrieve the name typed by the user on the form 
+	if (xmlHttp.readyState == 4 || xmlHttp.readyState == 0) {
+		// retrieve the name typed by the user on the form
+	
 		name = encodeURIComponent(document.getElementById("myName").value);
-		// execute the quickstart.php page from the server
-		xmlHttp.open("GET", ""quickstart.php?name=" + name, true);
-		// define the method to handle server respones
+		// execute the quickstart.php page the server
+		xmlHttp.open("GET", "quickstart.php?name=" + name, false); 
+		// define the method to handle server responses
 		xmlHttp.onreadystatechange = handleServerResponse;
+
 		// make the server request
 		xmlHttp.send(null);
+
+		// read the response
+		//handleServerResponse();
 	} else {
-		// if the connection is busy, try agin after one second
-		setTime("process()", 1000);
+		// if the connection is busy, try again after one second
+		setTimeout("process()", 1000);
 	}
 }
 
+/**
+ * callback function executed when a message is received from the server
+ */
 function handleServerResponse()
 {
-	// move forward only if the transaction has completed
-	if (xmlHttp.readyState == 4) {
-		// status of 200 indicates the transaction completed 
-		// successfully
-		if (xmlHttp.status == 200) {
-			// extract the XML retrieved from the server
-			xmlResponse = xmlHttp.responeXML;
-			// obtain the document element (the root element) on the XML
-			// structure
+	// move forward only if the trabsaction has completed
+	if (xmlHttp.status == 200) {
+		// extract the XML retrieved from the server
+		xmlResponse = xmlHttp.responseXML;
+
+		if (xmlResponse) {
+			// obtain the document element (the root element) of the the xml stucture
 			xmlDocumentElement = xmlResponse.documentElement;
-			// get the text message, which is in the first child of
-			// the the document element
+			// get the text message, which is in the first child og the document element
 			helloMessage = xmlDocumentElement.firstChild.data;
-			// display the data received from the server
+			// display the data received form the server
 			document.getElementById("divMessage").innerHTML = '<i>' + helloMessage + '</i>';
 			// restart sequence
 			setTimeout("process()", 1000);
-		} else {
-			alert("There was a problem accessing the server: " + xmlHttp.statusText);
 		}
+		
+	} else // a HTTP status different the 200 signals an error 
+	{
+		alert("There was a problem accessing the server: " + xmlHttp.statusText);
 	}
 }
