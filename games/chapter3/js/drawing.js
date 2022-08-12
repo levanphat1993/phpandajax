db5df524ba7f57f4f42186bd6a51fb23e1ca1f22
DESKTOP-6GUG76T\Amdin <levanphatit1993@gmail.com>
2022-08-12 20:51:59 +0700

    create method [login, signup, verify user]

---
+++ games/chapter3/js/drawing.js
@@ -0,0 +1,47 @@
+function draw_grid(ctx, minor, major, stroke, fill) {
+  minor = minor || 10;
+  major = major || minor * 5;
+  stroke = stroke || "#00FF00";
+  fill = fill || "#009900";
+  ctx.save();
+  ctx.strokeStyle = stroke;
+  ctx.fillStyle = fill;
+  let width = ctx.canvas.width,
+    height = ctx.canvas.height;
+  for (var x = 0; x < width; x += minor) {
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, height);
+    ctx.lineWidth = x % major == 0 ? 0.5 : 0.25;
+    ctx.stroke();
+    if (x % major == 0) {
+      ctx.fillText(x, x, 10);
+    }
+  }
+  for (var y = 0; y < height; y += minor) {
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(width, y);
+    ctx.lineWidth = y % major == 0 ? 0.5 : 0.25;
+    ctx.stroke();
+    if (y % major == 0) {
+      ctx.fillText(y, 0, y + 10);
+    }
+  }
+  ctx.restore();
+}
+
+function draw_pacman(ctx, x, y, r, m) {
+  angle = 0.2 * Math.PI * m;
+  ctx.save();
+  ctx.fillStyle = "yellow";
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 0.5;
+  ctx.beginPath();
+  ctx.arc(x, y, r, angle, -angle);
+  ctx.lineTo(x, y);
+  ctx.closePath()
+  ctx.fill();
+  ctx.stroke();
+  ctx.restore();
+}
